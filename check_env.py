50db4381df5be563c94d5facd520c25e4c432e42
HailAngel <an.pimiento@duocuc.cl>
2026-04-20 21:16:31 +0000

    añadido check_env

---
+++ check_env.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+print("--- Verificación de Entorno ---")
+print(f"Versión de Python: {sys.version}")
+print(f"Directorio actual: {os.getcwd()}")
+print("¡El entorno está listo!")
